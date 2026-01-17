5dc03d56db588c5629b9252c2cbd8c127e98bd45
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2026-01-17 19:20:34 -0300

    docs: adicionando os stories do Header

---
+++ src/components/layout/stories/Header.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Header } from '../page/Header'
+import { createRootRoute, createRouter, RouterProvider } from '@tanstack/react-router'
+import { useEffect } from 'react'
+
+// Wrapper to provide router context
+const DataRouterProvider = ({ children }: { children: React.ReactNode }) => {
+    const rootRoute = createRootRoute({
+        component: () => <>{children}</>
+    })
+    const router = createRouter({ routeTree: rootRoute })
+    return <RouterProvider router={router} />
+}
+
+const meta = {
+    title: 'Layout/Header',
+    component: Header,
+    parameters: {
+        layout: 'fullscreen',
+    },
+    decorators: [
+        (Story) => (
+            <DataRouterProvider>
+                <Story />
+            </DataRouterProvider>
+        )
+    ],
+    tags: ['autodocs'],
+} satisfies Meta<typeof Header>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}

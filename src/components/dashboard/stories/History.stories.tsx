7dd483c0d4a8dd4b6034f9dcee2c85a30460a345
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2026-01-17 19:21:26 -0300

    docs: adicionando todos os stories da tela de dashboard inicial de monitoramento

---
+++ src/components/dashboard/stories/History.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { History } from '../page/History'
+
+const meta = {
+    title: 'Dashboard/History',
+    component: History,
+    parameters: {
+        layout: 'padded',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof History>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        isLoading: false
+    }
+}
+
+export const Loading: Story = {
+    args: {
+        isLoading: true
+    }
+}
---
+++ src/components/dashboard/stories/RxPower.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { RxPower } from '../page/RxPower'
+
+const meta = {
+    title: 'Dashboard/RxPower',
+    component: RxPower,
+    parameters: {
+        layout: 'padded',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof RxPower>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        isLoading: false
+    }
+}
+
+export const Loading: Story = {
+    args: {
+        isLoading: true
+    }
+}

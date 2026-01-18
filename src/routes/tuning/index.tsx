065ed13eabe4242dc8d9b0339df94586ca2d71fd
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2026-01-18 20:45:14 -0300

    fix: atualizando o uso da store na chamada do componente de forma correta

--- src/routes/tuning/index.tsx
+++ src/routes/tuning/index.tsx
@@ -5,6 +5,7 @@ import { calibrationStore } from '@/stores/calibrationStore'
 import { CalibrationMode } from '@/components/tuning/page/CalibrationMode'
 import { CalibrationInfoColumn } from '@/components/tuning/page/CalibrationInfoColumn'
 import { ExternalCalibrationForm } from '@/components/tuning/page/ExternalCalibrationForm'
+import { loadingStore } from '@/stores/loadingStore'
 
 export const Route = createFileRoute('/tuning/')({
   component: RouteComponent,
@@ -16,6 +17,7 @@ function RouteComponent() {
   const [activeMode, setActiveMode] = useState<'internal' | 'external'>(
     'internal',
   )
+  const isLoading = useStore(loadingStore)
 
   return (
     <div className="min-h-max bg-zinc-950">
@@ -27,12 +29,13 @@ function RouteComponent() {
               activeMode={activeMode}
               onModeChange={setActiveMode}
               status={status}
+              isLoading={isLoading}
             />
           </div>
 
           {/* Col 2: Info (4 cols) */}
           <div className="lg:col-span-4 h-full">
-            <CalibrationInfoColumn />
+            <CalibrationInfoColumn isLoading={isLoading} />
           </div>
 
           {/* Col 3: Form (5 cols) */}
@@ -42,6 +45,7 @@ function RouteComponent() {
               txPower={txPower}
               temperature={temperature}
               info={info}
+              isLoading={isLoading}
             />
           </div>
         </div>
